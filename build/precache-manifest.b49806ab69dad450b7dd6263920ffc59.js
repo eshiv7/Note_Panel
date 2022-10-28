3dc955dd826c8f65f52cbd46909517ec62c5569f
shivraj <shivraj.rajput.777@gmail.com>
2022-10-28 20:34:54 +0530

    update

---
+++ build/precache-manifest.b49806ab69dad450b7dd6263920ffc59.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8368e589e8fa59a3784d081f083054",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36df98cfedea4f0f47c6",
+    "url": "/static/js/2.240d069a.chunk.js"
+  },
+  {
+    "revision": "afa9ce6f68319afc467b",
+    "url": "/static/js/main.76256714.chunk.js"
+  },
+  {
+    "revision": "84832877ed76605d723e",
+    "url": "/static/js/runtime-main.aa743321.js"
+  }
+]);
